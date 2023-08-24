e47f11f8ceebe03891cba084d12cacdc2593e0af
LamaAsta <amalformal@gmail.com>
2023-08-24 20:29:04 +0530

    Added remove roll. Little issue with async

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 import discord
 
-token = ""
+token = "MTEzMzM1NDEyMzI2MjE3NzQwMA.GwXhEa._nCBFbMyqm9SAp6ahPO7wyfFBRSTTOIC8fuhrs"
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='&',intents=intents)
 #has_any_role will come in handy
@@ -13,28 +13,36 @@ def run():
         if not ctx.invoked_subcommand:
             await ctx.send("Please give a command")
     @roleRequest.command()
-
     async def assign(ctx,mention:discord.Member,role:str):
 
         r = discord.utils.get(ctx.guild.roles, name=role)
         if r:
             if role in roles:
-                await mention.add_roles(r)
-                await ctx.send("{} has been given role {} by {} :)".format(mention,r,ctx.author))
+                if r in mention.roles:
+                    await mention.add_roles(r)
+                    await ctx.send("{} has been given role {} by {} :)".format(mention,r,ctx.author))
+                else:
+                    await ctx.send("{} Alread has the role >:(".format(mention))
             else:
                 await ctx.send("The assignment of that role if beyond my capabilities :(")
         else:
             await ctx.send("Pls give a valid role ")
     
+    @roleRequest.command()
+    async def remove(ctx,mention:discord.Member, role:str):
+        r = discord.utils.get(ctx.guild.roles, name=role)
+        if r:
+            if role in roles:
+                if r in mention.roles:
+                    await mention.remove_roles(r)
+                    await ctx.send("{} has removed role {} by {} :)".format(mention,r,ctx.author))
+                else:
+                    await ctx.send("{} doesnt have that role to begin with ".format(mention))
+            else:
+                await ctx.send("The assignment of that role if beyond my capabilities :(")
+        else:
+            await ctx.send("Pls give a valid role ")
     bot.run(token)
 
 run()       
 
-# @bot.command()
-# async def assign_role(ctx, member: discord.Member, role_name: str):
-#     role = discord.utils.get(ctx.guild.roles, name=role_name)
-#     if role:
-#         await member.add_roles(role)
-#         await ctx.send(f"Assigned {role.name} role to {member.display_name}")
-#     else:
-#         await ctx.send("Role not found.")
